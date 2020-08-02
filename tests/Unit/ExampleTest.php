eb95f057739cf7be6bb3fe8f8469afe5aec3acda
lacasera <aboateng62@gmail.com>
2020-08-02 01:00:58 +0000

    ch(): fix tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,9 +3,12 @@
 it('should get app version', function() {
     $this->get('/v1/health');
 
-    $this->assertEquals(
-        $this->app->version(),
-        $this->response->getContent()
-    );
+    $this->assertEquals([
+            "message" => "api is healthy",
+            "apiVersion" => "v1",
+            "lumenVersion" => "Lumen (7.2.1) (Laravel Components ^7.0)"
+        ],
 
+        json_decode($this->response->getContent(), true)
+    );
 }); 
